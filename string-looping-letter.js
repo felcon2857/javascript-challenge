b6d5ef4e3de722fd79a37d9123d5aa932cf9a80d
Felcon Albaladejo <68620593+felcon2857@users.noreply.github.com>
2021-12-09 20:43:43 +0800

    string-looping-letter function
    A javascript fundamental using. Basic data segregation by the use of array map.

---
+++ string-looping-letter.js
@@ -0,0 +1,15 @@
+// segregate the letters and repeat accordingly to each element.
+/** example : 
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"*/
+
+// function example
+function accum(s) {
+  const myArray = s
+    .split("")
+    .map((x, index) => x.toUpperCase() + Array(index + 1).join(x.toLowerCase()))
+    .join("-");
+  console.log(myArray);
+}
+accum("ZpglnRxqenU");
